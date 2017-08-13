481b8c83322012d97f937ecdd84919e5034aa8f7
Scott Pavio <scottpavio@gmail.com>
2017-08-12 20:40:52 -0400

    Objects

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ switch (job) {
     default:
         console.log('Scott does something else');
            }
-
+           
 
 
 //Coding Challenge 1
@@ -234,4 +234,44 @@ if (john.indexOf('teacher') === -1) {
 */
 
 ///////Lecture: Objects
+
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: '1990',
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+//data mutation
+
+john.lastName = 'Miller';
+john['job'] = 'programmer';
+
+console.log(john);
+
+//new object
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+
+
+
+
+
+
+
+
 
